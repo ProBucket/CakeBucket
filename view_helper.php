7348a263f8a8fb3b5c71b024e829225b3af2abeb
ProBucket <hasan.khan.pro@gmail.com>
2017-01-30 16:53:31 +0600

    Update view_helper.php

--- view_helper.php
+++ view_helper.php
@@ -1,5 +1,9 @@
 <?php 
-
+    
+    // Url path
     $this->Url->build('/organization-signup', true);
+
+    // Link
+    $this->Html->link('Text',['controller'=>'events', 'action' => 'index'],['class' => 'btn', 'escapeTitle' => false, 'title' => 'Cancel']);
 
   
